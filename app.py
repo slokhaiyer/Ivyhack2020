948adcb6f4514a62d61482acf9b87e2e5512447a
Slokha Iyer <65013187+slokhaiyer@users.noreply.github.com>
2020-10-05 00:14:05 +0530

    Landing b.e

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def html():
-    return render_template('index.html')
+def hello_world():
+    return render_template('landing.html')
 
-
-
+if __name__ == '__main__':
+    app.run(debug=True)
